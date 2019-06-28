a32195f262d4ff3c57aac444a0aa654b21d3cd08
Deonte Cooper <deonte.cooper12@gmail.com>
2019-06-28 01:07:06 -0600

    Added Session rout to return user api_key

---
+++ routes/api/v1/sessions.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../../../models').User;
+const bcrypt = require('bcrypt');
+pry = require('pryjs');
+
+// GET Exisiting User
+
+router.post('/', function(req, res, next) {
+	User.findAll({
+		where: {
+			email: req.body.email
+		}
+	})
+	.then(user => {
+		// eval(pry.it)
+		if (bcrypt.compareSync(req.body.password, user[0].password)) {
+			res.setHeader('Content-type','application/json');
+			res.status(200).send(JSON.stringify({ "api_key": user[0].apiKey }))
+		}
+	})
+	.catch(error => {
+		res.setHeader("Content-Type", "application/json");
+		res.status(501).send(JSON.stringify({ message: error.errors[0].message }));
+	});
+	;
+
+
+module.exports = router;
