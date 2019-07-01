1b9e71d6e5c1bf0b01a99a0e4449fc130f281c5e
Deonte Cooper <deonte.cooper12@gmail.com>
2019-06-30 22:16:58 -0600

    Added get route for all favorites

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var User = require('../../../models').User;
 var FavoriteCities = require('../../../models').FavoriteCities;
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+var Forecast = require('../../../services');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+const { darkSkyApi, googleApi } = require('../../../config.js');
 pry = require('pryjs');
 
 var findUser = function(apiKey) {
@@ -13,6 +16,60 @@ var findUser = function(apiKey) {
     },
   })
 }
+var findFavorites = function(userId) {
+  return FavoriteCities.findAll({
+    where: {
+      'UserId': userId
+    },
+  })
+}
+
+var findFavoriteCitiesWeather = function(favcities) {
+  let fav = favcities.map(function(element) {
+    let formatedLocation = formatLocation(element);
+    let forecast = new Forecast({location: formatedLocation});
+    return forecast.googleService()
+    .then(coords => {
+      return forecast.darkskyService(coords.geometry.location);
+    })
+    .then( weather => {
+      return forecast.currentWeather(weather, element.location);
+    }).then(forecast => {
+      return forecast
+    })
+  })
+  return fav
+}
+
+var formatLocation = function(element) {
+  return element.location.toLowerCase().replace(/\s+/g, '');
+}
+let favoriteCities = [{location: "Irvine, CA"}, {location: 'Denver, CO'}]
+
+
+// GET favorite cities
+router.get('/', function(req, res, next) {
+  if (req.body.hasOwnProperty('api_key')){
+    findUser(req.body.api_key)
+    .then(user => {
+      findFavorites(user[0].id)
+      .then(favoriteCities => {
+        Promise.all(findFavoriteCitiesWeather(favoriteCities))
+        .then(favorite => {
+          res.setHeader('Content-type','application/json');
+          res.status(201).send(JSON.stringify(favorite))
+        })
+      })
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(501).send(JSON.stringify({ message: error.errors[0].message }));
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({message: 'Unauthorized User'}));
+  }
+});
 
 // POST New favorite city
 router.post('/', function(req, res, next) {
@@ -28,9 +85,9 @@ router.post('/', function(req, res, next) {
         .then(favorite => {
           res.setHeader('Content-type','application/json');
           res.status(201).send(JSON.stringify({ message: `${favorite.location} has been added to your favorites` }))
+        })
       })
     })
-  })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
       res.status(501).send(JSON.stringify({ message: error.errors[0].message }));
@@ -40,5 +97,6 @@ router.post('/', function(req, res, next) {
     res.status(401).send(JSON.stringify({message: 'Unauthorized User'}));
   }
 });
+
 
 module.exports = router;
